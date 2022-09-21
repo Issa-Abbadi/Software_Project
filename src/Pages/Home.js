5b94f5c20f44bf3c3b49c92be4f6b44c549b2872
Issa Abbadi <71350660+Issa-Abbadi@users.noreply.github.com>
2022-09-21 14:26:27 +0300

    edit home

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./Home.css";
 
 const pic = `<div class="card">
 <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
@@ -18,14 +19,122 @@ const pic = `<div class="card">
 </div>
 </div>`;
 
+const header = `<header>
+
+
+<!-- Background image -->
+<div
+  class="p-5 text-center bg-image"
+  style="
+    background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
+    height: 550px;
+  "
+>
+  <div class="mask " style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="d-flex justify-content-center align-items-center" style="  color:black;  height: 100%;
+    width: 100%;
+    object-fit: contain;
+    position: relative;
+    top: 0;
+    left: 0;">
+      <div class="text-white">
+        <h1 class="mb-3">Heading</h1>
+        <h4 class="mb-3">Subheading</h4>
+        <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+        >Call to action</a
+        >
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Background image -->
+</header>`;
+
 function Home() {
   return (
     <>
+      <Container>
+        <Row sm={7}>
+          <div dangerouslySetInnerHTML={{ __html: header }} />
+        </Row>
+      </Container>
       <Container>
         <Row>
           <Col>
             <div dangerouslySetInnerHTML={{ __html: pic }} />
           </Col>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+        </Row>{" "}
+        <Row>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+        </Row>{" "}
+        <Row>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            {" "}
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+        </Row>{" "}
+        <Row>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+          <Col>
+            <div dangerouslySetInnerHTML={{ __html: pic }} />
+          </Col>
+
           <Col>
             {" "}
             <div dangerouslySetInnerHTML={{ __html: pic }} />
