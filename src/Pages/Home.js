a2c7cf3f9b02efb916c2a917146b0b43b7380321
Issa Abbadi <71350660+Issa-Abbadi@users.noreply.github.com>
2022-09-21 18:29:12 +0300

    some css

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+
 import Container from "react-bootstrap/Container";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./Home.css";
 
-const pic = `<div class="card">
+const pic = `
+
+
+<div class="card">
 <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
   <img src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.webp" class="img-fluid"/>
   <a href="#!">
@@ -19,45 +24,22 @@ const pic = `<div class="card">
 </div>
 </div>`;
 
-const header = `<header>
-
-
-<!-- Background image -->
-<div
-  class="p-5 text-center bg-image"
-  style="
-    background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
-    height: 550px;
-  "
->
-  <div class="mask " style="background-color: rgba(0, 0, 0, 0.6);">
-    <div class="d-flex justify-content-center align-items-center" style="  color:black;  height: 100%;
-    width: 100%;
-    object-fit: contain;
-    position: relative;
-    top: 0;
-    left: 0;">
-      <div class="text-white">
-        <h1 class="mb-3">Heading</h1>
-        <h4 class="mb-3">Subheading</h4>
-        <a class="btn btn-outline-light btn-lg" href="#!" role="button"
-        >Call to action</a
-        >
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Background image -->
-</header>`;
-
 function Home() {
   return (
     <>
       <Container>
-        <Row sm={7}>
-          <div dangerouslySetInnerHTML={{ __html: header }} />
+        <Row>
+          <Col sm={7}>
+            <div class="img-container" style={{ "max-width": "22rem" }}>
+              <img
+                src="https://mdbootstrap.com/img/new/standard/city/053.webp"
+                class="w-100"
+              />
+            </div>
+          </Col>
         </Row>
       </Container>
+
       <Container>
         <Row>
           <Col>
@@ -149,7 +131,4 @@ function Home() {
   );
 }
 
-{
-  /*  */
-}
 export default Home;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+//
+// import "mdb-react-ui-kit/dist/css/mdb.min.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./index.css";
 import App from "./App";
