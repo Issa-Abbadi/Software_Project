1c851e9c9037f5b3d5fb58cb8dc9b648ee0f9b9e
Ahmad-Qorom <qorom.h.ahmad@gmail.com>
2022-10-11 09:18:16 +0300

    css in signin

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -21,19 +21,32 @@ function LoginForm(props) {
         <Col
           // md={{ span: 4, offset: 4 }}
           style={{
-            "background-color": "#eee",
-            padding: "2rem",
-            borderRadius: "5%",
-            marginTop: "20px",
+            //   "background-color": "#eee",
+            // padding: "2rem",
+            // borderRadius: "5%",
+            // marginTop: "20px",  
+            // "background-color": "blue",
+            // "display": "flex",
+            // "align-items": "center",
+            // "flex-direction": "column",
+            // "height": "80vh",
+            // "width": "30vh",
+            // "background": "rgba(255,255,255,0.1)",
+            // "Box-shadow": "0 8px 32px 0 rgba(31,38,135,0.37)",
+            // "backdrop-filter": "blur(8.5px)",
+            // "border-radius": "10px", 
+            // "color": "#ffffff",
+            // "text-transform": "uppercase",
+            // "letter-spacing": "0.1rem"
           }}
         >
           <div className="form-wrapper outcard">
             <Formik {...props} validationSchema={validationSchema}>
-              <Form class = "login">
-                <h1>تسجيل الدخول</h1>
+              <Form class = "loginForm">
+                <h1 class = "loginTitle">تسجيل الدخول</h1>
                 <FormGroup class = "login-title">
-                  <label htmlFor="email">الإيميل</label>
-                  <Field name="email" type="email" className="form-control" placeholder="الإيميل"/>
+                  <label htmlFor="email">البريد الالكتروني</label>
+                  <Field name="email" type="email" className="form-control" placeholder="البريد الالكتروني"/>
                   <ErrorMessage
                     name="email"
                     className="d-block invalid-feedback"
@@ -58,42 +71,42 @@ function LoginForm(props) {
                     <label htmlFor="remeber">تذكرني</label>
                     <input id="remeber" type="checkbox" />
                   </span>
-                    <Link
-                  class="text-reset"
-                  style={{
-                    textAlign: "center",
-                    display: "inline-block",
-                    marginTop: "5px",
-                    marginLeft: "5px",
-                    marginRight: "5px",
-                  }}
-                  to={"/forget-pass"}
-                >
-                  {" "}
-                  نسيت كلمة السر ؟{" "}
+                <Link
+                      class="text-reset"
+                      style={{
+                        textAlign: "center",
+                        display: "inline-block",
+                        marginTop: "5px",
+                        marginLeft: "5px",
+                        marginRight: "5px",
+                      }}
+                      to={"/forget-pass"}
+                    >
+                      {" "}
+                      نسيت كلمة السر ؟{" "}
                 </Link>
                 <Button
-                  style={{display: "block" , width: "100%" , marginTop: "5%" }}
-                  variant="dark"
-                  size="lg"
-                  block="block"
-                  type="submit"
-                >
-                  {props.children}
-                </Button>
+                      style={{display: "block" , width: "100%" , marginTop: "5%" }}
+                      variant="dark"
+                      size="lg"
+                      block="block"
+                      type="submit"
+                    >
+                      {props.children}
+                    </Button>
                 <Link
-                  class="text-reset"
-                  style={{
-                    textAlign: "center",
-                    display: "inline-block",
-                    marginTop: "5px",
-                    marginLeft: "5px",
-                    marginRight: "5px",
-                  }}
-                  to={"/signup"}
-                >
-                  {" "}
-                  SIGN UP{" "}
+                      class="text-reset"
+                      style={{
+                        textAlign: "center",
+                        display: "inline-block",
+                        marginTop: "5px",
+                        marginLeft: "5px",
+                        marginRight: "5px",
+                      }}
+                      to={"/signup"}
+                    >
+                      {" "}
+                      SIGN UP{" "}
                 </Link>
                
               </Form>
@@ -104,5 +117,6 @@ function LoginForm(props) {
     </>
   );
 }
+
 
 export default LoginForm;
