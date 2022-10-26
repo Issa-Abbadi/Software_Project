51ccd3ffe35188052b9ca188b7681921ac772af1
Issa Abbadi <71350660+Issa-Abbadi@users.noreply.github.com>
2022-10-26 21:13:11 +0300

    edit css on sign up page

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,86 +1,183 @@
 import React, { useState } from "react";
 import * as Yup from "yup";
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import { FormGroup, Container, Col, Button } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
+import { FormGroup, Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/system";
+import Container from "@mui/material/Container";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email("You have enter an invalid email address")
+    .required("Required"),
+  password: Yup.string()
+    .required("No password provided.")
+    .min(8, "Password is too short - should be 8 chars minimum.")
+    .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+});
+const useStyles = styled((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 function SignUpForm(props) {
-  const validationSchema = Yup.object().shape({
-    email: Yup.string()
-      .email("You have enter an invalid email address")
-      .required("Required"),
-    password: Yup.string()
-      .required("No password provided.")
-      .min(8, "Password is too short - should be 8 chars minimum.")
-      .matches(/[a-zA-Z]/, "Password can only contain Latin letters."),
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      email: "foobar@example.com",
+      password: "foobar",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (studentObject) => {
+      axios
+        .post("http://localhost:4000/signup/", studentObject)
+        .then((res) => {
+          if (res.status === 200) {
+            alert("Signup success.");
+            navigate("/login");
+          } else Promise.reject();
+        })
+        .catch((err) => alert("Something went wrong"));
+    },
   });
+
   console.log(props);
   return (
     <>
-      <Container>
-        <Col
-          md={{ span: 4, offset: 4 }}
-          style={{
-            "background-color": "#eee",
-            padding: "2rem",
-            borderRadius: "5%",
-            marginTop: "20px",
-          }}
-        >
-          <div className="form-wrapper outcard">
-            <Formik {...props} validationSchema={validationSchema}>
-              <Form>
-                <h1>SIGNUP</h1>
-                <FormGroup>
-                  <label htmlFor="email">Email</label>
-                  <Field name="email" type="email" className="form-control" />
-                  <ErrorMessage
+      <div className="form-wrapper outcard">
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <form className={classes.form} onSubmit={formik.handleSubmit}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="fname"
+                    name="firstName"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="lname"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
                     name="email"
-                    className="d-block invalid-feedback"
-                    component="span"
+                    autoComplete="email"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
                   />
-                </FormGroup>
-                <FormGroup>
-                  <label htmlFor="password">Password</label>
-                  <Field
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    required
+                    fullWidth
                     name="password"
+                    label="Password"
                     type="password"
-                    className="form-control"
+                    id="password"
+                    autoComplete="current-password"
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                    error={formik.touched.email && Boolean(formik.errors.email)}
+                    helperText={formik.touched.email && formik.errors.email}
                   />
-                  <ErrorMessage
-                    name="password"
-                    className="d-block invalid-feedback"
-                    component="span"
+                </Grid>
+                <Grid item xs={12}>
+                  <FormControlLabel
+                    control={
+                      <Checkbox value="allowExtraEmails" color="primary" />
+                    }
+                    label="I want to receive inspiration, marketing promotions and updates via email."
                   />
-                </FormGroup>
-                <Button
-                  style={{ margin: "2rem", display: "block" }}
-                  variant="dark"
-                  size="lg"
-                  block="block"
-                  type="submit"
-                >
-                  {props.children}
-                </Button>
-                <Link
-                  class="text-reset"
-                  style={{
-                    textAlign: "center",
-                    display: "inline-block",
-                    marginTop: "5px",
-                    marginLeft: "5px",
-                    marginRight: "5px",
-                  }}
-                  to={"/login"}
-                >
-                  {" "}
-                  Login{" "}
-                </Link>
-              </Form>
-            </Formik>
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+              <Grid container justifyContent="flex-end">
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    Already have an account? Sign in
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
           </div>
-        </Col>
-      </Container>
+          <Box mt={5}></Box>
+        </Container>
+      </div>
     </>
   );
 }
